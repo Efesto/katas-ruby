c13eda00c61949d346a91f1386a3377f990dd6da
Marco "Efesto" Polita <polita.marco@gmail.com>
2016-01-20 15:59:01 +0100

    WIP got bug with strike bonus after strike

--- bowling_game/bowling_play.rb
+++ bowling_game/bowling_play.rb
@@ -1,24 +1,56 @@
 class BowlingPlay
 
-  attr_reader :current_score, :current_pins, :current_throws
+  MAX_FRAMES = 10
 
   def initialize
-    @current_score = 0
-    @current_pins = 10
-    @current_throws = 0
+    @current_frame = 0
+    @frames = []
+    @frame_throw = 0
   end
 
   def throw_ball
-    if @current_throws % 2 == 0
-      @current_pins = 10
+    throw_pins_hit = pins_hit
+
+    if @frames[@current_frame].nil?
+      @frames << [throw_pins_hit]
+    else
+      @frames[@current_frame] << throw_pins_hit
     end
 
-    pins_knocked_out = pins_hit
-
-    @current_score = @current_score + pins_knocked_out
-    @current_pins = @current_pins - pins_knocked_out
-    @current_throws = @current_throws + 1
+    @frame_throw = @frame_throw == 0 && throw_pins_hit < 10 ? 1 : 0
+    @current_frame = @current_frame + 1 if @frame_throw == 0
   end
+
+  def total_score
+    (0..MAX_FRAMES - 1).inject { |sum, frame| sum + score(frame)}
+  end
+
+  def score frame
+    simple_score(frame) + spare_bonus(frame) + strike_bonus(frame)
+  end
+
+  def simple_score(frame)
+    @frames[frame].inject { |sum, score| sum + score }
+  end
+
+  def spare_bonus frame
+    if @frames[frame].length == 2 && simple_score(frame) == 10 && @frames.length > frame + 1
+      @frames[frame + 1][0]
+    else
+      0
+    end
+  end
+
+  def strike_bonus frame
+    bonus = 0
+    if @frames[frame].length == 1 && simple_score(frame) == 10 && @frames.length > frame + 1
+      next_frame = @frames[frame + 1]
+      bonus = next_frame[0]
+      bonus = bonus + next_frame[1] if next_frame.length == 2
+    end
+    bonus
+  end
+
 
   def pins_hit
 
--- bowling_game/bowling_test.rb
+++ bowling_game/bowling_test.rb
@@ -7,38 +7,60 @@ class BowlingTest < Test::Unit::TestCase
     @bowling_play = BowlingPlay.new
   end
 
-  def test_frame_score_simple
-
-    BowlingPlay.any_instance.stubs(:pins_hit).returns(1)
-
-    assert_equal 0, @bowling_play.current_score
-    assert_equal 10, @bowling_play.current_pins
-    assert_equal 0, @bowling_play.current_throws
+  def test_frames_score_spare
+    pins_hit 1, 9, 5
 
     @bowling_play.throw_ball
-    assert_equal 1, @bowling_play.current_score
-    assert_equal 9, @bowling_play.current_pins
-    assert_equal 1, @bowling_play.current_throws
+    assert_equal 1, @bowling_play.score(0)
 
     @bowling_play.throw_ball
-    assert_equal 2, @bowling_play.current_score
-    assert_equal 8, @bowling_play.current_pins
-    assert_equal 2, @bowling_play.current_throws
+    assert_equal 10, @bowling_play.score(0)
 
     @bowling_play.throw_ball
-    assert_equal 3, @bowling_play.current_score
-    assert_equal 9, @bowling_play.current_pins
-    assert_equal 3, @bowling_play.current_throws
+    assert_equal 15, @bowling_play.score(0)
+    assert_equal 5, @bowling_play.score(1)
   end
 
-  def test_frame_score_spare
-    BowlingPlay.any_instance.stubs(:pins_hit).returns(5,5)
+  def test_frames_score_strike
+    pins_hit 10, 8, 1
 
     @bowling_play.throw_ball
+    assert_equal 10, @bowling_play.score(0)
+
     @bowling_play.throw_ball
-    assert_equal 0, @bowling_play.current_pins
-    assert_equal 15, @bowling_play.current_score
+    assert_equal 18, @bowling_play.score(0)
+    assert_equal 8, @bowling_play.score(1)
+
+    @bowling_play.throw_ball
+    assert_equal 19, @bowling_play.score(0)
+    assert_equal 9, @bowling_play.score(1)
+  end
+
+  def test_strike_bonus_after_strike
+    pins_hit(10,10,10)
+    3.times { @bowling_play.throw_ball }
+
+    assert_equal 30, @bowling_play.score(0)
+  end
 
 
+  def test_last_frame_strike
+
+    pins_hit(10,10,10,10,10,10,10,10,10,10,1,1)
+    10.times { @bowling_play.throw_ball}
+
+    assert_equal 10, @bowling_play.score(9)
+
+    @bowling_play.throw_ball
+    assert_equal 11, @bowling_play.score(9)
+
+    @bowling_play.throw_ball
+    assert_equal 12, @bowling_play.score(9)
+    assert_equal 275, @bowling_play.total_score
+  end
+
+
+  def pins_hit(*pins)
+    BowlingPlay.any_instance.stubs(:pins_hit).returns(*pins)
   end
 end
